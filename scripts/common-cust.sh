0c7e929c1957eccd76ebe886b4292fa1e5e4fec3
George Kargiotakis <kargig@noc.grnet.gr>
2015-09-01 12:18:59 +0300

    Add isolation capabilities to bridged networks

--- scripts/common-cust.sh
+++ scripts/common-cust.sh
@@ -18,6 +18,12 @@ function setup_firewall_ganetimgr {
     subchain=vima_$INTERFACE
 
     # collect info...
+    # 20150831 This has been commented out because we now need isolation for
+    #          bridged vlans as well.
+    #if [[ "$LINK" = public* ]]; then
+    #    IS_PUBLIC=1
+    #fi
+
     if [[ "$LINK" = public* ]]; then
         IS_PUBLIC=1
     fi
@@ -42,35 +48,44 @@ function setup_firewall_ganetimgr {
 
     # ...and make the decision
     # bailout early on non-public links
-    test -z $IS_PUBLIC && return
+    # 20150831 This has been commented out because we now need isolation for
+    #          bridged vlans as well.
+    #test -z $IS_PUBLIC && return
 
     # better safe than sorry
     mkdir -p "$GANETI_FERM"
     rm -f $GANETI_FERM/$INTERFACE
     if [ $VIMA_FIREWALL -eq 1 ]; then
+        # create different ferm configuration depending on bridged/routed mode
+        if [ "$MODE" = "routed" ]; then
+            FERM_MODE='interface'
+        elif [ "$MODE" = "bridged" ]; then
+            FERM_MODE='mod physdev physdev-in'
+        fi
+
         # Create a single jump because multiple @subchain declarations
         # create multiple '-N subchain' rules which result in error.
         # But we need at least a single "chain $subchain" rule applied to create
         # the necessary subchain. Else jump fails. That is the SERVICE_MAIL rule.
         cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip ip6) chain FORWARD { interface $INTERFACE jump "$subchain"; }
+    domain (ip ip6) chain FORWARD { $FERM_MODE $INTERFACE jump "$subchain"; }
 EOF
         if [ -z "$SERVICE_MAIL" ]; then
             cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip ip6) chain "$subchain" interface $INTERFACE {
+    domain (ip ip6) chain "$subchain" $FERM_MODE $INTERFACE {
         proto tcp dport 25 REJECT;
     }
 EOF
         else # Can't have empty jump (chain X, creates chain if it doesn't exist)
             cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip ip6) chain "$subchain" interface $INTERFACE {
+    domain (ip ip6) chain "$subchain" $FERM_MODE $INTERFACE {
         proto tcp dport 25 ACCEPT;
     }
 EOF
         fi  
         if [ -n "$WHITELIST_IPV6" ]; then
             cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip6) chain "$subchain" interface $INTERFACE {
+    domain (ip6) chain "$subchain" $FERM_MODE $INTERFACE {
         proto all saddr $WHITELIST_IPV6 ACCEPT;
         proto all daddr $WHITELIST_IPV6 ACCEPT;
     }
@@ -78,7 +93,7 @@ EOF
         fi  
         if [ -n "$WHITELIST_IPV4" ]; then
             cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip) chain "$subchain" interface $INTERFACE {
+    domain (ip) chain "$subchain" $FERM_MODE $INTERFACE {
         proto all saddr $WHITELIST_IPV4 ACCEPT;
         proto all daddr $WHITELIST_IPV4 ACCEPT;
     }
@@ -86,7 +101,7 @@ EOF
         fi  
         if [ -n "$ISOLATE" ]; then
             cat >>"$GANETI_FERM/$INTERFACE" <<EOF
-    domain (ip ip6) chain "$subchain" interface $INTERFACE {
+    domain (ip ip6) chain "$subchain" $FERM_MODE $INTERFACE {
         DROP;
     }
 EOF
